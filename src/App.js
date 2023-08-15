6d6bcaff9a0f6a0fac5fc027d117141936fa0175
MSA\nkolli <nkolli@miraclesoft.com>
2023-08-15 22:14:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// import './App.css';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Emplisting from './EmployeeListing';
+// import Empcreate from './EmployeeCreate';
+// import Empdetails from './EmployeeDetails';
+// import Empedit from './EmployeeEdit';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>React JS CRUD Operations</h1>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path='/' element={<Emplisting />}></Route>
+//           <Route path='/employeedetails' element={<EmployeeCreate />}></Route>
+//           <Route path='/employeedetails/details/:empid' element={<EmployeeDetails />}></Route>
+//           <Route path='/employeedetails/edit/:empid' element={<EmployeeEdit />}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React JS CRUD Operations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employeedetails/create' element={<EmpCreate />}></Route>
+          <Route path='/employeedetails/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employeedetails/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
